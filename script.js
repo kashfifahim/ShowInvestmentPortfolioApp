57d0925108be1429cf3f028ccb8030a4b5c02d37
Kashfi Fahim <kashfi.fahim@gmail.com>
2023-12-16 10:53:05 -0500

    Add single-page application to display real-time APPL stock price
    - Implemented basic HTML stucture with a dedicated section for displaying the stock price.
    - Added CSS for styling the webpage, focusing on readability and simple layout.
    - Developed JS functionality to fetch real-time APPLE stock price using Alpha Vantage API.
    - Handled API responses and erorrs, displaying stock price or error message accordingly.

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoad', function() {
+    fetchStockPrice();
+});
+
+
+function fetchStockPrice() {
+	const apiKey = "Z26IND1A7LZTHK7V";
+	const symbol = 'AAPL';
+	const url = 'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${apiKey}';
+
+	fetch(url)
+		.then(response => response.json())
+		.then(data => {
+			const price = data['Global Quote']['05. price'];
+			document.getElementById('stock-price').innerText = '$${parseFloat(price).toFixed(2)}';})
+		.catch(error => {
+			console.error('Error fetching stock data:', erorr);
+			document.getElementById('stock-price').innerText = 'Sorry Problem Loading Data';
+		});
+}
